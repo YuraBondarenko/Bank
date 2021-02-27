64719e2c2b265e7d0b9d6bd4e9d88675ec8320c3
YuraBondarenko <yuriybondarenko2001@gmail.com>
2021-02-27 21:09:13 +0200

    added model for User and Role

---
+++ src/main/java/com/example/bankservice/model/Role.java
@@ -0,0 +1,27 @@
+package com.example.bankservice.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "role_name")
+    @Enumerated(EnumType.STRING)
+    private RoleName roleName;
+
+    public enum RoleName {
+        ADMIN, USER
+    }
+}
